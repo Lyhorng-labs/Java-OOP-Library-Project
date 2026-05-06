27f12be11214d058fc0f6283d411badc0b82d83a
Lyhorng Heng <lyhorngheng086@gmail.com>
2026-05-06 09:21:25 -0500

    feat: add TeacherUser subclass with department and borrow limit.

---
+++ src/TeacherUser.java
@@ -0,0 +1,15 @@
+public class TeacherUser extends User{
+    private String department;
+
+    public TeacherUser(String name, int id, String department){
+        super(name, id);
+        this.department= department;
+    }
+
+    public int getBorrowLimit(){
+        return 10;
+    }
+    public String getDepartment(){
+        return department;
+    }
+}
