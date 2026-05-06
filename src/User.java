ad143372c9b630aed27020d8608bd94d4208b753
Lyhorng Heng <lyhorngheng086@gmail.com>
2026-05-06 09:04:00 -0500

    Refactor: convert User to abstract class and implement Borrowable interface.

--- src/User.java
+++ src/User.java
@@ -1,46 +1,23 @@
 import java.util.ArrayList;
-public class User {
+public abstract class User implements Borrowable {
     private String name;
-    private int id;
-    private ArrayList<Book> borrowbook;
+    private long id;
+    protected ArrayList<Book> borrowbook;
 
-    public User(String name, int id){
+    public User(String name, long id){
         this.name=name;
         this.id=id;
         this.borrowbook=new ArrayList<>();
     }
-
+    public abstract int getBorrowLimit();
     public String getName(){
         return name;
     }
 
-    public int getId(){
+    public long getId(){
         return id;
     }
 
-    public ArrayList<Book> getBorrowbook(){
-        return borrowbook;
-    }
-
-    public void borrowBook(Book book){
-        if (book.isAvailable()){
-            borrowbook.add(book);
-            book.setAvailable(false);
-            System.out.println(name + " borrowed " + book.getTitle() + ".");
-        } else {
-            System.out.println(book.getTitle() + " is not available.");
-        }
-    }
-    public void returnBook(Book book){
-        if (borrowbook.contains(book)){
-            borrowbook.remove(book);
-            book.setAvailable(true);
-            System.out.println("User: " + name + " returned " + book.getTitle() + ".");
-        }else{
-            System.out.println("User: " + name + " does not have this book in their borrow list.");
-        }
-    }
-    @Override
     public String toString(){
         return "User: " + name + " (ID: " + id + ") has " + borrowbook.size() + " books.";
     }
